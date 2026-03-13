216ed016a746c60af4c54484afe9044d7cb01ce3
Jacob Celestine <jacobceles@gmail.com>
2026-03-13 19:05:54 -0400

    fixed issues with host and directory

--- stock_analysis_mcp/constants.py
+++ stock_analysis_mcp/constants.py
@@ -2,7 +2,7 @@ NSE_HOST_URL = "https://www.nseindia.com"
 METADATA_ENDPOINT = "/api/quote-equity"
 HISTORICAL_ENDPOINT = "/api/historicalOR/generateSecurityWiseHistoricalData"
 
-DUMP_DIR = "./agent/data"
+DUMP_DIR = "./data"
 
 REDDIT_SUBREDDITS = ["IndianStockMarket", "IndianStreetBets", "StockMarketIndia"]
 REDDIT_POST_LIMIT = 30
